5a915213b18eb5ff74abdeac80e86bbbb21227df
Patrik-24 <128226181+Patrik-24@users.noreply.github.com>
2023-04-08 18:42:34 +0200

    Add files via upload

---
+++ rebScript.js
@@ -0,0 +1,18 @@
+let colors = document.querySelectorAll(".green-lime")
+
+colors.forEach(element => {
+    console.log(element.innerText);
+
+    if(element.innerText > 92){
+        colors.style.color = "lime";
+    }
+
+    const value = parseFloat(element.innerText);
+        if (value >= 92) {
+        element.style.color = 'lime';
+    }
+});
+
+function showValue(value) {
+    document.getElementById("rangeValue").innerHTML = value;
+}
